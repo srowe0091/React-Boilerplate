5abc052d3d1e83d7861113674122f51d2280176d
Steven Rowe <srowe0091@gmail.com>
2024-01-06 23:08:56 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from '/react.svg'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React Static Boilerplate</h1>
+    </>
+  )
+}
+
+export default App
